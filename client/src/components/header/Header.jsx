aa35860f7f00850b199de2665185acb86ce652d0
Boris Kalapchiev <boriskalapchiev@gmail.com>
2025-04-08 11:24:01 +0300

    Add dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,25 +1,36 @@
+import { useContext } from "react";
 import { Link } from "react-router";
 
+import { UserContext } from "../../contexts/UserContext";
+
 export default function Header() {
+    const { username } = useContext(UserContext);
+
     return (
         <header>
             <h1 className="headingLogo">
                 BikeRush
             </h1>
-            
+
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/search">Search</Link>
                 <Link to="/listings">All Listings</Link>
-                <div id="user">
-                    <Link to="/listings/create">Create Listing</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
 
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {username
+                    ? (
+                        <div id="user">
+                            <Link to="/listings/create">Create Listing</Link>
+                            <Link to="/logout">Logout</Link>
+                        </div>
+                    )
+                    : (
+                        <div id="guest">
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </div>
+                    )
+                }
             </nav>
         </header>
     );
